f3c59e7e26608365e97f50186ecd137fe9e80760
DuneSciFye <dunescifye@gmail.com>
2024-08-09 12:17:47 -0400

    Added amount arg to SetCursorItem

--- src/main/java/me/dunescifye/commandutils/commands/SetCursorItem.java
+++ src/main/java/me/dunescifye/commandutils/commands/SetCursorItem.java
@@ -1,22 +1,25 @@
 package me.dunescifye.commandutils.commands;
 
-import dev.jorel.commandapi.CommandTree;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.IntegerArgument;
 import dev.jorel.commandapi.arguments.ItemStackArgument;
 import dev.jorel.commandapi.arguments.PlayerArgument;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class SetCursorItem {
     
     public static void register() {
-        new CommandTree("setcursoritem")
-            .then(new PlayerArgument("Player")
-                .then(new ItemStackArgument("Item")
-                    .executes((sender, args) -> {
-                        Player p = args.getUnchecked("Player");
-                        p.setItemOnCursor(args.getUnchecked("Item"));
-                    })
-                )
-            )
+        new CommandAPICommand("setcursoritem")
+            .withArguments(new PlayerArgument("Player"))
+            .withArguments(new ItemStackArgument("Item"))
+            .withOptionalArguments(new IntegerArgument("Amount", 0, 64))
+            .executes((sender, args) -> {
+                Player p = args.getUnchecked("Player");
+                ItemStack item = args.getUnchecked("Item");
+                item.setAmount(args.getOrDefaultUnchecked("Amount", 1));
+                p.setItemOnCursor(item);
+            })
             .withPermission("commandutils.command.setcursoritem")
             .register("commandutils");
     }
