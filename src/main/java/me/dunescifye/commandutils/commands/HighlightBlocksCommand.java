c3754f7147a49d33cf4d9c8c02049136a790f334
DuneSciFye <65089425+DuneSciFye@users.noreply.github.com>
2024-08-18 18:56:15 -0400

    Work on HighlightBlocksCommand

--- src/main/java/me/dunescifye/commandutils/commands/HighlightBlocksCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/HighlightBlocksCommand.java
@@ -2,8 +2,10 @@ package me.dunescifye.commandutils.commands;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandTree;
 import dev.jorel.commandapi.arguments.*;
 import dev.jorel.commandapi.wrappers.ParticleData;
+import me.dunescifye.commandutils.files.Config;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
@@ -11,6 +13,7 @@ import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 import static me.dunescifye.commandutils.CommandUtils.getInstance;
@@ -51,8 +54,9 @@ public class HighlightBlocksCommand extends Command implements Configurable {
         IntegerArgument radiusArg = new IntegerArgument("Radius", 0);
         BlockPredicateArgument blockArg = new BlockPredicateArgument("Block");
         ParticleArgument particleArg = new ParticleArgument("Particle");
+        StringArgument whitelistedBlocksArgument = new StringArgument("Whitelisted Blocks");
 
-        if (!multipleBlocks) {
+        if (multipleBlocks) {
             new CommandAPICommand("highlightblocks")
                 .withArguments(locArg)
                 .withArguments(worldArg)
@@ -94,6 +98,66 @@ public class HighlightBlocksCommand extends Command implements Configurable {
                         }
                     }
                 })
+                .withPermission(this.getPermission())
+                .withAliases(this.getCommandAliases())
+                .register(this.getNamespace());
+        } else {
+            new CommandTree("highlightblocks")
+                .then(locArg
+                    .then(worldArg
+                        .then(radiusArg
+                            .then(whitelistedBlocksArgument
+                                .replaceSuggestions(ArgumentSuggestions.strings(Config.getWhitelistKeySet()))
+                                .then(particleArg
+                                    .executes((sender, args) -> {
+                                        World world = Bukkit.getWorld(args.getByArgument(worldArg));
+                                        Location location = args.getByArgument(locArg);
+                                        Block origin = world.getBlockAt(location);
+                                        int radius = args.getByArgument(radiusArg);
+                                        ParticleData<?> particleData = args.getByArgument(particleArg);
+                                        Particle particle = particleData == null ? Particle.valueOf(stringParticle) : particleData.particle();
+                                        String whitelistedBlocks = args.getByArgument(whitelistedBlocksArgument);
+                                        List<Predicate<Block>> whitelist = Config.getWhitelist(whitelistedBlocks), blacklist = Config.getBlacklist(whitelistedBlocks);
+
+
+                                        for (int x = -radius; x <= radius; x++) {
+                                            for (int y = -radius; y <= radius; y++) {
+                                                block:
+                                                for (int z = -radius; z <= radius; z++) {
+                                                    Block relative = origin.getRelative(x, y, z);
+                                                    for (Predicate<Block> predicateWhitelist : whitelist) {
+                                                        if (predicateWhitelist.test(relative)) {
+                                                            for (Predicate<Block> predicateBlacklist : blacklist) {
+                                                                if (predicateBlacklist.test(relative)) {
+                                                                    continue block;
+                                                                }
+                                                            }
+                                                            new BukkitRunnable() {
+                                                                int count = 0;
+
+                                                                @Override
+                                                                public void run() {
+                                                                    if (count >= 10) {
+                                                                        cancel();
+                                                                        return;
+                                                                    }
+
+                                                                    world.spawnParticle(particle, origin.getX() + 0.5, origin.getY() + 0.5, origin.getZ() + 0.5, 3, 0, 0, 0, 0);
+
+                                                                    count += 1;
+                                                                }
+                                                            }.runTaskTimer(getInstance(), 0, 5);
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    })
+                                )
+                            )
+                        )
+                    )
+                )
                 .withPermission(this.getPermission())
                 .withAliases(this.getCommandAliases())
                 .register(this.getNamespace());
