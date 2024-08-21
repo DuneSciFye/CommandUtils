910e558e3963910c34d9bcd3d133be033a98e6b3
DuneSciFye <dunescifye@gmail.com>
2024-08-21 14:22:51 -0400

    Cleanup code

--- src/main/java/me/dunescifye/commandutils/commands/BreakAndReplantCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/BreakAndReplantCommand.java
@@ -31,8 +31,8 @@ public class BreakAndReplantCommand extends Command implements Registerable {
         BlockStateArgument blockArg = new BlockStateArgument("Original Block");
 
         new CommandTree("breakandreplant")
-            .then(locArg
-                .then(worldArg
+            .then(worldArg
+                .then(locArg
                     .then(playerArg
                         .then(radiusArg
                             .then(blockArg
@@ -103,6 +103,79 @@ public class BreakAndReplantCommand extends Command implements Registerable {
 
                             })
                         )
+                    )
+                )
+            )
+            .then(locArg
+                .then(playerArg
+                    .then(radiusArg
+                        .then(blockArg
+                            .executes((sender, args) -> {
+                                World world = Bukkit.getWorld(args.getByArgument(worldArg));
+                                Location location = args.getByArgument(locArg);
+                                Block block = world.getBlockAt(location);
+                                BlockData original = args.getByArgument(blockArg);
+                                int radius = args.getByArgument(radiusArg);
+                                Player player = args.getByArgument(playerArg);
+                                ItemStack heldItem = player.getInventory().getItemInMainHand();
+
+                                Collection<ItemStack> drops = new ArrayList<>();
+
+                                block.setType(original.getMaterial());
+
+                                for (int x = -radius; x <= radius; x++){
+                                    for (int z = -radius; z <= radius; z++){
+                                        Block b = block.getRelative(x, 0, z);
+                                        BlockData blockData = b.getBlockData();
+                                        if (blockData instanceof Ageable ageable) {
+                                            Collection<ItemStack> blockDrops = b.getDrops(heldItem);
+                                            for (ItemStack drop : blockDrops) {
+                                                if (drop.getType().equals(ageable.getPlacementMaterial())) drop.setAmount(drop.getAmount() - 1);
+                                            }
+                                            drops.addAll(blockDrops);
+                                            ageable.setAge(0);
+                                            b.setBlockData(ageable);
+                                        }
+                                    }
+                                }
+
+                                for (ItemStack item : mergeSimilarItemStacks(drops)){
+                                    world.dropItemNaturally(location, item);
+                                }
+                            })
+                        )
+                    )
+                    .then(new BlockStateArgument("Original Block")
+                        .executes((sender, args) -> {
+                            World world = Bukkit.getWorld(args.getByArgument(worldArg));
+                            Location location = args.getByArgument(locArg);
+                            Block block = world.getBlockAt(location);
+                            BlockData original = args.getByArgument(blockArg);
+                            Player player = args.getByArgument(playerArg);
+                            ItemStack heldItem = player.getInventory().getItemInMainHand();
+
+
+                            block.setType(original.getMaterial());
+                            Collection<ItemStack> drops = new ArrayList<>();
+
+                            BlockData blockData = block.getBlockData();
+                            if (blockData instanceof Ageable) {
+                                if (block.getType() != Material.SUGAR_CANE) {
+                                    Collection<ItemStack> blockDrops = block.getDrops(heldItem);
+                                    for (ItemStack drop : blockDrops) {
+                                        if (drop.getType().equals(blockData.getPlacementMaterial()))
+                                            drop.setAmount(drop.getAmount() - 1);
+                                    }
+                                    drops.addAll(blockDrops);
+                                    ((Ageable) blockData).setAge(0);
+                                    block.setBlockData(blockData);
+                                }
+                                for (ItemStack item : drops){
+                                    world.dropItemNaturally(location, item);
+                                }
+                            }
+
+                        })
                     )
                 )
             )
--- src/main/java/me/dunescifye/commandutils/commands/BroadcastMessageCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/BroadcastMessageCommand.java
@@ -1,19 +1,58 @@
 package me.dunescifye.commandutils.commands;
 
+import dev.dejvokep.boostedyaml.YamlDocument;
 import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.BooleanArgument;
 import dev.jorel.commandapi.arguments.GreedyStringArgument;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-public class BroadcastMessageCommand extends Command implements Registerable {
+public class BroadcastMessageCommand extends Command implements Configurable {
 
     @SuppressWarnings("ConstantConditions")
-    public void register() {
+    public void register(YamlDocument config) {
+
         if (!this.getEnabled()) return;
 
+        boolean ampersandByDefault, parsePlaceholdersByDefault, colorCodesByDefault;
+
+        if (config.getOptionalString("Commands.SendMessage.Use&ForColorCodesByDefault").isPresent()) {
+            if (config.isString("Commands.SendMessage.Use&ForColorCodesByDefault")) {
+                ampersandByDefault = config.getBoolean("Commands.SendMessage.Use&ForColorCodesByDefault");
+            } else {
+                ampersandByDefault = true;
+            }
+        } else {
+            ampersandByDefault = true;
+            config.set("Commands.SendMessage.Use&ForColorCodesByDefault", true);
+        }
+
+        if (config.getOptionalString("Commands.SendMessage.ParsePlaceholdersByDefault").isPresent()) {
+            if (config.isString("Commands.SendMessage.ParsePlaceholdersByDefault")) {
+                parsePlaceholdersByDefault = config.getBoolean("Commands.SendMessage.ParsePlaceholdersByDefault");
+            } else {
+                parsePlaceholdersByDefault = true;
+            }
+        } else {
+            parsePlaceholdersByDefault = true;
+            config.set("Commands.SendMessage.ParsePlaceholdersByDefault", true);
+        }
+
+        if (config.getOptionalString("Commands.SendMessage.ColorCodesByDefault").isPresent()) {
+            if (config.isString("Commands.SendMessage.ColorCodesByDefault")) {
+                colorCodesByDefault = config.getBoolean("Commands.SendMessage.ColorCodesByDefault");
+            } else {
+                colorCodesByDefault = true;
+            }
+        } else {
+            colorCodesByDefault = true;
+            config.set("Commands.SendMessage.ColorCodesByDefault", true);
+        }
+
         GreedyStringArgument messageArg = new GreedyStringArgument("Message");
+        BooleanArgument
 
         new CommandAPICommand("broadcastmessage")
             .withArguments(messageArg)
