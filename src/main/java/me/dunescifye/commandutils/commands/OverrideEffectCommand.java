3c7730efcb2e12ff4965d7350287ca4c95521b44
DuneSciFye <dunescifye@gmail.com>
2024-08-23 10:38:30 -0400

    Work on effect commands

--- src/main/java/me/dunescifye/commandutils/commands/OverrideEffectCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/OverrideEffectCommand.java
@@ -2,15 +2,12 @@ package me.dunescifye.commandutils.commands;
 
 import dev.dejvokep.boostedyaml.YamlDocument;
 import dev.jorel.commandapi.CommandAPICommand;
-import dev.jorel.commandapi.CommandTree;
 import dev.jorel.commandapi.arguments.*;
-import me.dunescifye.commandutils.CommandUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.HashMap;
-import java.util.logging.Logger;
 
 public class OverrideEffectCommand extends Command implements Configurable {
 
@@ -21,20 +18,6 @@ public class OverrideEffectCommand extends Command implements Configurable {
     public void register(YamlDocument config) {
 
         if (!this.getEnabled()) return;
-
-        Logger logger = CommandUtils.getInstance().getLogger();
-
-        boolean infiniteDuration;
-
-        if (config.getOptionalBoolean("Commands.PreciseEffect.InfiniteDuration").isEmpty()) {
-            config.set("Commands.PreciseEffect.InfiniteDuration", true);
-        }
-        if (config.isBoolean("Commands.PreciseEffect.InfiniteDuration")) {
-            infiniteDuration = config.getBoolean("Commands.PreciseEffect.InfiniteDuration");
-        } else {
-            infiniteDuration = true;
-            logger.warning("Configuration option Commands.PreciseEffect.InfiniteDuration is not a boolean! Found " + config.getString("Commands.PreciseEffect.InfiniteDuration"));
-        }
 
         PlayerArgument playerArg = new PlayerArgument("Player");
         MultiLiteralArgument functionArg = new MultiLiteralArgument("Function", "override", "retrieve", "remove");
@@ -84,60 +67,56 @@ public class OverrideEffectCommand extends Command implements Configurable {
                             player.addPotionEffect(potionEffect);
                         }
                     }
-                    case "remove" -> {
+                    case "remove" ->
                         potionEffects.remove(id);
-                    }
                 }
             })
             .withPermission(this.getPermission())
             .withAliases(this.getCommandAliases())
             .register(this.getNamespace());
 
-        if (infiniteDuration) {
-            new CommandAPICommand("overrideeffect")
-                .withArguments(functionArg)
-                .withArguments(idArg)
-                .withArguments(playerArg)
-                .withArguments(effectArg)
-                .withOptionalArguments(infiniteArg)
-                .withOptionalArguments(amplifierArg)
-                .withOptionalArguments(hideParticlesArg)
-                .withOptionalArguments(ambientArg)
-                .executes((sender, args) -> {
-                    Player player = args.getByArgument(playerArg);
-                    String id = args.getByArgument(idArg);
-                    PotionEffectType effectType = args.getByArgument(effectArg);
-                    int amplifier = args.getByArgumentOrDefault(amplifierArg, 0);
-                    boolean hideParticles = args.getByArgumentOrDefault(hideParticlesArg, false);
-                    boolean ambient = args.getByArgumentOrDefault(ambientArg, false);
-                    boolean icon = args.getByArgumentOrDefault(iconArg, true);
+        new CommandAPICommand("overrideeffect")
+            .withArguments(functionArg)
+            .withArguments(idArg)
+            .withArguments(playerArg)
+            .withArguments(effectArg)
+            .withOptionalArguments(infiniteArg)
+            .withOptionalArguments(amplifierArg)
+            .withOptionalArguments(hideParticlesArg)
+            .withOptionalArguments(ambientArg)
+            .executes((sender, args) -> {
+                Player player = args.getByArgument(playerArg);
+                String id = args.getByArgument(idArg);
+                PotionEffectType effectType = args.getByArgument(effectArg);
+                int amplifier = args.getByArgumentOrDefault(amplifierArg, 0);
+                boolean hideParticles = args.getByArgumentOrDefault(hideParticlesArg, false);
+                boolean ambient = args.getByArgumentOrDefault(ambientArg, false);
+                boolean icon = args.getByArgumentOrDefault(iconArg, true);
 
-                    switch (args.getByArgument(functionArg)) {
-                        case "override" -> {
-                            if (player.hasPotionEffect(effectType)) {
-                                PotionEffect potionEffect = player.getPotionEffect(effectType);
-                                if (!potionEffects.containsKey(id)) {
-                                    potionEffects.put(id, potionEffect);
-                                }
+                switch (args.getByArgument(functionArg)) {
+                    case "override" -> {
+                        if (player.hasPotionEffect(effectType)) {
+                            PotionEffect potionEffect = player.getPotionEffect(effectType);
+                            if (!potionEffects.containsKey(id)) {
+                                potionEffects.put(id, potionEffect);
                             }
+                        }
 
-                            PotionEffect potionEffect = new PotionEffect(effectType, PotionEffect.INFINITE_DURATION, amplifier, ambient, hideParticles, icon);
+                        PotionEffect potionEffect = new PotionEffect(effectType, PotionEffect.INFINITE_DURATION, amplifier, ambient, hideParticles, icon);
+                        player.addPotionEffect(potionEffect);
+                    }
+                    case "retrieve" -> {
+                        PotionEffect potionEffect = potionEffects.remove(id);
+                        if (potionEffect != null) {
                             player.addPotionEffect(potionEffect);
                         }
-                        case "retrieve" -> {
-                            PotionEffect potionEffect = potionEffects.remove(id);
-                            if (potionEffect != null) {
-                                player.addPotionEffect(potionEffect);
-                            }
-                        }
-                        case "remove" -> {
-                            potionEffects.remove(id);
-                        }
                     }
-                })
-                .withPermission(this.getPermission())
-                .withAliases(this.getCommandAliases())
-                .register(this.getNamespace());
-        }
+                    case "remove" ->
+                        potionEffects.remove(id);
+                }
+            })
+            .withPermission(this.getPermission())
+            .withAliases(this.getCommandAliases())
+            .register(this.getNamespace());
     }
 }
--- src/main/java/me/dunescifye/commandutils/commands/PreciseEffectCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/PreciseEffectCommand.java
@@ -3,12 +3,9 @@ package me.dunescifye.commandutils.commands;
 import dev.dejvokep.boostedyaml.YamlDocument;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.*;
-import me.dunescifye.commandutils.CommandUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-
-import java.util.logging.Logger;
 
 public class PreciseEffectCommand extends Command implements Configurable {
 
@@ -18,20 +15,6 @@ public class PreciseEffectCommand extends Command implements Configurable {
     public void register(YamlDocument config) {
 
         if (!this.getEnabled()) return;
-
-        Logger logger = CommandUtils.getInstance().getLogger();
-
-        boolean infiniteDuration;
-
-        if (config.getOptionalBoolean("Commands.PreciseEffect.InfiniteDuration").isEmpty()) {
-            config.set("Commands.PreciseEffect.InfiniteDuration", true);
-        }
-        if (config.isBoolean("Commands.PreciseEffect.InfiniteDuration")) {
-            infiniteDuration = config.getBoolean("Commands.PreciseEffect.InfiniteDuration");
-        } else {
-            infiniteDuration = true;
-            logger.warning("Configuration option Commands.PreciseEffect.InfiniteDuration is not a boolean! Found " + config.getString("Commands.PreciseEffect.InfiniteDuration"));
-        }
 
         PlayerArgument playerArg = new PlayerArgument("Player");
         PotionEffectArgument effectArg = new PotionEffectArgument("Effect");
@@ -65,28 +48,27 @@ public class PreciseEffectCommand extends Command implements Configurable {
             .withAliases(this.getCommandAliases())
             .register(this.getNamespace());
 
-        if (infiniteDuration) {
-            new CommandAPICommand("preciseeffect")
-                .withArguments(playerArg)
-                .withArguments(effectArg)
-                .withOptionalArguments(infiniteArg)
-                .withOptionalArguments(amplifierArg)
-                .withOptionalArguments(hideParticlesArg)
-                .withOptionalArguments(ambientArg)
-                .executes((sender, args) -> {
-                    Player player = args.getByArgument(playerArg);
-                    PotionEffectType effectType = args.getByArgument(effectArg);
-                    int amplifier = args.getByArgumentOrDefault(amplifierArg, 0);
-                    boolean hideParticles = args.getByArgumentOrDefault(hideParticlesArg, false);
-                    boolean ambient = args.getByArgumentOrDefault(ambientArg, false);
-                    boolean icon = args.getByArgumentOrDefault(iconArg, true);
+        //Infinite Effect
+        new CommandAPICommand("preciseeffect")
+            .withArguments(playerArg)
+            .withArguments(effectArg)
+            .withOptionalArguments(infiniteArg)
+            .withOptionalArguments(amplifierArg)
+            .withOptionalArguments(hideParticlesArg)
+            .withOptionalArguments(ambientArg)
+            .executes((sender, args) -> {
+                Player player = args.getByArgument(playerArg);
+                PotionEffectType effectType = args.getByArgument(effectArg);
+                int amplifier = args.getByArgumentOrDefault(amplifierArg, 0);
+                boolean hideParticles = args.getByArgumentOrDefault(hideParticlesArg, false);
+                boolean ambient = args.getByArgumentOrDefault(ambientArg, false);
+                boolean icon = args.getByArgumentOrDefault(iconArg, true);
 
-                    PotionEffect potionEffect = new PotionEffect(effectType, PotionEffect.INFINITE_DURATION, amplifier, ambient, hideParticles, icon);
-                    player.addPotionEffect(potionEffect);
-                })
-                .withPermission(this.getPermission())
-                .withAliases(this.getCommandAliases())
-                .register(this.getNamespace());
-        }
+                PotionEffect potionEffect = new PotionEffect(effectType, PotionEffect.INFINITE_DURATION, amplifier, ambient, hideParticles, icon);
+                player.addPotionEffect(potionEffect);
+            })
+            .withPermission(this.getPermission())
+            .withAliases(this.getCommandAliases())
+            .register(this.getNamespace());
     }
 }
