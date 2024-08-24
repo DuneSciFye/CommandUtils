a4d414696caa2e3d8e47450701e628077e2d6b59
DuneSciFye <dunescifye@gmail.com>
2024-08-24 19:58:48 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/BlockCycleCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/BlockCycleCommand.java
@@ -120,79 +120,72 @@ public class BlockCycleCommand extends Command implements Registerable {
                 Material material = b.getType();
                 BlockData blockData = b.getBlockData();
 
+                Stairs.Shape shape = null;
+                boolean waterlogged = false;
+                BlockFace facing = null;
+                Bisected.Half half = null;
+                Slab.Type type = null;
+
                 if (blockData instanceof Stairs stairs) {
-                    Stairs.Shape shape = stairs.getShape();
-                    boolean waterlogged = stairs.isWaterlogged();
-                    org.bukkit.block.BlockFace facing = stairs.getFacing();
-
-                    switch (material) {
-                        case CUT_COPPER_STAIRS -> b.setType(Material.EXPOSED_CUT_COPPER_STAIRS);
-                        case EXPOSED_CUT_COPPER_STAIRS -> b.setType(Material.WEATHERED_CUT_COPPER_STAIRS);
-                        case WEATHERED_CUT_COPPER_STAIRS -> b.setType(Material.OXIDIZED_CUT_COPPER_STAIRS);
-                        case OXIDIZED_CUT_COPPER_STAIRS -> b.setType(Material.CUT_COPPER_STAIRS);
-                        case WAXED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_EXPOSED_CUT_COPPER_STAIRS);
-                        case WAXED_EXPOSED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_WEATHERED_CUT_COPPER_STAIRS);
-                        case WAXED_WEATHERED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_OXIDIZED_CUT_COPPER_STAIRS);
-                        case WAXED_OXIDIZED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_CUT_COPPER_STAIRS);
-                    }
-
-                    // Get the new block data
-                    BlockData newBlockData = b.getBlockData();
-
-                    // Set the facing direction and other properties
-                    if (newBlockData instanceof Stairs newStairs) {
-                        newStairs.setFacing(facing);
-                        newStairs.setShape(shape);
-                        newStairs.setWaterlogged(waterlogged);
-                        b.setBlockData(newStairs);
-                    }
+                    shape = stairs.getShape();
+                    waterlogged = stairs.isWaterlogged();
+                    facing = stairs.getFacing();
+                    half = stairs.getHalf();
                 } else if (blockData instanceof Slab slab) {
-                    Slab.Type type = slab.getType();
-                    boolean waterlogged = slab.isWaterlogged();
-
-                    switch (material) {
-                        case CUT_COPPER_SLAB -> b.setType(Material.EXPOSED_CUT_COPPER_SLAB);
-                        case EXPOSED_CUT_COPPER_SLAB -> b.setType(Material.WEATHERED_CUT_COPPER_SLAB);
-                        case WEATHERED_CUT_COPPER_SLAB -> b.setType(Material.OXIDIZED_CUT_COPPER_SLAB);
-                        case OXIDIZED_CUT_COPPER_SLAB -> b.setType(Material.CUT_COPPER_SLAB);
-                        case WAXED_CUT_COPPER_SLAB -> b.setType(Material.WAXED_EXPOSED_CUT_COPPER_SLAB);
-                        case WAXED_EXPOSED_CUT_COPPER_SLAB ->
-                            b.setType(Material.WAXED_WEATHERED_CUT_COPPER_SLAB);
-                        case WAXED_WEATHERED_CUT_COPPER_SLAB ->
-                            b.setType(Material.WAXED_OXIDIZED_CUT_COPPER_SLAB);
-                        case WAXED_OXIDIZED_CUT_COPPER_SLAB -> b.setType(Material.WAXED_CUT_COPPER_SLAB);
-                    }
-
-                    // Get the new block data
-                    BlockData newBlockData = b.getBlockData();
-
-                    // Set the slab type and waterlogged state
-                    if (newBlockData instanceof Slab newSlab) {
-                        newSlab.setType(type);
-                        newSlab.setWaterlogged(waterlogged);
-                        b.setBlockData(newSlab);
-                    }
-                } else {
-                    switch (material) {
-                        case COPPER_BLOCK -> b.setType(Material.EXPOSED_COPPER);
-                        case EXPOSED_COPPER -> b.setType(Material.WEATHERED_COPPER);
-                        case WEATHERED_COPPER -> b.setType(Material.OXIDIZED_COPPER);
-                        case OXIDIZED_COPPER -> b.setType(Material.COPPER_BLOCK);
-                        case CUT_COPPER -> b.setType(Material.EXPOSED_CUT_COPPER);
-                        case EXPOSED_CUT_COPPER -> b.setType(Material.WEATHERED_CUT_COPPER);
-                        case WEATHERED_CUT_COPPER -> b.setType(Material.OXIDIZED_CUT_COPPER);
-                        case OXIDIZED_CUT_COPPER -> b.setType(Material.CUT_COPPER);
-                        case WAXED_COPPER_BLOCK -> b.setType(Material.WAXED_EXPOSED_COPPER);
-                        case WAXED_EXPOSED_COPPER -> b.setType(Material.WAXED_WEATHERED_COPPER);
-                        case WAXED_WEATHERED_COPPER -> b.setType(Material.WAXED_OXIDIZED_COPPER);
-                        case WAXED_OXIDIZED_COPPER -> b.setType(Material.WAXED_COPPER_BLOCK);
-                        case WAXED_CUT_COPPER -> b.setType(Material.WAXED_EXPOSED_CUT_COPPER);
-                        case WAXED_EXPOSED_CUT_COPPER -> b.setType(Material.WAXED_WEATHERED_CUT_COPPER);
-                        case WAXED_WEATHERED_CUT_COPPER -> b.setType(Material.WAXED_OXIDIZED_CUT_COPPER);
-                        case WAXED_OXIDIZED_CUT_COPPER -> b.setType(Material.WAXED_CUT_COPPER);
-                    }
+                    type = slab.getType();
+                    waterlogged = slab.isWaterlogged();
                 }
 
+                switch (material) {
+                    case CUT_COPPER_STAIRS -> b.setType(Material.EXPOSED_CUT_COPPER_STAIRS);
+                    case EXPOSED_CUT_COPPER_STAIRS -> b.setType(Material.WEATHERED_CUT_COPPER_STAIRS);
+                    case WEATHERED_CUT_COPPER_STAIRS -> b.setType(Material.OXIDIZED_CUT_COPPER_STAIRS);
+                    case OXIDIZED_CUT_COPPER_STAIRS -> b.setType(Material.CUT_COPPER_STAIRS);
+                    case WAXED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_EXPOSED_CUT_COPPER_STAIRS);
+                    case WAXED_EXPOSED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_WEATHERED_CUT_COPPER_STAIRS);
+                    case WAXED_WEATHERED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_OXIDIZED_CUT_COPPER_STAIRS);
+                    case WAXED_OXIDIZED_CUT_COPPER_STAIRS -> b.setType(Material.WAXED_CUT_COPPER_STAIRS);
+                    case CUT_COPPER_SLAB -> b.setType(Material.EXPOSED_CUT_COPPER_SLAB);
+                    case EXPOSED_CUT_COPPER_SLAB -> b.setType(Material.WEATHERED_CUT_COPPER_SLAB);
+                    case WEATHERED_CUT_COPPER_SLAB -> b.setType(Material.OXIDIZED_CUT_COPPER_SLAB);
+                    case OXIDIZED_CUT_COPPER_SLAB -> b.setType(Material.CUT_COPPER_SLAB);
+                    case WAXED_CUT_COPPER_SLAB -> b.setType(Material.WAXED_EXPOSED_CUT_COPPER_SLAB);
+                    case WAXED_EXPOSED_CUT_COPPER_SLAB ->
+                        b.setType(Material.WAXED_WEATHERED_CUT_COPPER_SLAB);
+                    case WAXED_WEATHERED_CUT_COPPER_SLAB ->
+                        b.setType(Material.WAXED_OXIDIZED_CUT_COPPER_SLAB);
+                    case WAXED_OXIDIZED_CUT_COPPER_SLAB -> b.setType(Material.WAXED_CUT_COPPER_SLAB);
+                    case COPPER_BLOCK -> b.setType(Material.EXPOSED_COPPER);
+                    case EXPOSED_COPPER -> b.setType(Material.WEATHERED_COPPER);
+                    case WEATHERED_COPPER -> b.setType(Material.OXIDIZED_COPPER);
+                    case OXIDIZED_COPPER -> b.setType(Material.COPPER_BLOCK);
+                    case CUT_COPPER -> b.setType(Material.EXPOSED_CUT_COPPER);
+                    case EXPOSED_CUT_COPPER -> b.setType(Material.WEATHERED_CUT_COPPER);
+                    case WEATHERED_CUT_COPPER -> b.setType(Material.OXIDIZED_CUT_COPPER);
+                    case OXIDIZED_CUT_COPPER -> b.setType(Material.CUT_COPPER);
+                    case WAXED_COPPER_BLOCK -> b.setType(Material.WAXED_EXPOSED_COPPER);
+                    case WAXED_EXPOSED_COPPER -> b.setType(Material.WAXED_WEATHERED_COPPER);
+                    case WAXED_WEATHERED_COPPER -> b.setType(Material.WAXED_OXIDIZED_COPPER);
+                    case WAXED_OXIDIZED_COPPER -> b.setType(Material.WAXED_COPPER_BLOCK);
+                    case WAXED_CUT_COPPER -> b.setType(Material.WAXED_EXPOSED_CUT_COPPER);
+                    case WAXED_EXPOSED_CUT_COPPER -> b.setType(Material.WAXED_WEATHERED_CUT_COPPER);
+                    case WAXED_WEATHERED_CUT_COPPER -> b.setType(Material.WAXED_OXIDIZED_CUT_COPPER);
+                    case WAXED_OXIDIZED_CUT_COPPER -> b.setType(Material.WAXED_CUT_COPPER);
+                }
+
+                BlockData newBlockData = b.getBlockData();
+
+                if (newBlockData instanceof Stairs newStairs) {
+                    newStairs.setFacing(facing);
+                    newStairs.setShape(shape);
+                    newStairs.setWaterlogged(waterlogged);
+                    newStairs.setHalf(half);
+                    b.setBlockData(newStairs);
+                } else if (newBlockData instanceof Slab newSlab) {
+                    newSlab.setType(type);
+                    newSlab.setWaterlogged(waterlogged);
+                    b.setBlockData(newSlab);
+                }
             })
             .withPermission(this.getPermission())
             .withAliases(this.getCommandAliases())
@@ -210,6 +203,13 @@ public class BlockCycleCommand extends Command implements Registerable {
                 BlockData blockData = b.getBlockData();
                 String material = b.getType().toString();
                 Material newMat = Material.valueOf(material.startsWith("WAXED_") ? material.substring(6) : "WAXED_" + material);
+
+                Stairs.Shape shape = null;
+                boolean waterlogged = false;
+                BlockFace facing = null;
+                Bisected.Half half = null;
+                Slab.Type type = null;
+
                 if (blockData instanceof Stairs stairs) {
                     Stairs.Shape shape = stairs.getShape();
                     BlockFace facing = stairs.getFacing();
