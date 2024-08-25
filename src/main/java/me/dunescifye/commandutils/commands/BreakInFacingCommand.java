dcf6f4d6fb06fec3c31a23a147f18eb3d6de0520
DuneSciFye <65089425+DuneSciFye@users.noreply.github.com>
2024-08-25 13:22:03 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/BreakInFacingCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/BreakInFacingCommand.java
@@ -644,6 +644,105 @@ public class BreakInFacingCommand extends Command implements Registerable {
                 .register(this.getNamespace());
 
             /**
+             * Breaks Blocks in Direction Player is Facing with GriefPrevention, Define Predicates in List Format on Command
+             * @author DuneSciFye
+             * @since 1.0.0
+             * @param Location Location of the Center Block
+             * @param Player Player who is Breaking the Blocks
+             * @param X Direction in X to Break in
+             * @param Y Direction in Y to Break in
+             * @param Z Direction in Z to Break in
+             * @param Depth Number of Blocks to Break Forward in
+             * @param whitelist Literal Argument
+             * @param Predicates List of Predicates
+             */
+            new CommandAPICommand("breakinfacing")
+                .withArguments(locArg)
+                .withArguments(playerArg)
+                .withArguments(radiusArg)
+                .withArguments(depthArg)
+                .withArguments(whitelistArg)
+                .withArguments(new ListArgumentBuilder<String>("Whitelisted Blocks")
+                    .withList(Utils.getPredicatesList())
+                    .withStringMapper()
+                    .buildText())
+                .executes((sender, args) -> {
+                    List<Predicate<Block>> whitelist = new ArrayList<>(), blacklist = new ArrayList<>();
+                    Utils.stringListToPredicate(args.getUnchecked("Whitelisted Blocks"), whitelist, blacklist);
+
+                    Location location = args.getByArgument(locArg);
+                    Block origin = location.getBlock();
+                    int xRadius = args.getByArgument(xArg);
+                    int yRadius = args.getByArgument(yArg);
+                    int zRadius = args.getByArgument(zArg);
+                    Player player = args.getByArgument(playerArg);
+                    ItemStack heldItem = player.getInventory().getItemInMainHand();
+                    int depth = args.getByArgument(depthArg);
+
+                    depth = depth < 1 ? 1 : depth -1;
+                    double pitch = player.getLocation().getPitch();
+                    int xStart = -xRadius, yStart = -yRadius, zStart = -zRadius, xEnd = xRadius, yEnd = yRadius, zEnd = zRadius;
+                    if (pitch < -45) {
+                        yStart = 0;
+                        yEnd = depth;
+                    } else if (pitch > 45) {
+                        yStart = -depth;
+                        yEnd = 0;
+                    } else {
+                        switch (player.getFacing()) {
+                            case NORTH -> {
+                                zStart = -depth;
+                                zEnd = 0;
+                            }
+                            case SOUTH -> {
+                                zStart = 0;
+                                zEnd = depth;
+                            }
+                            case WEST -> {
+                                xStart = -depth;
+                                xEnd = 0;
+                            }
+                            case EAST -> {
+                                xStart = 0;
+                                xEnd = depth;
+                            }
+                        }
+                    }
+
+                    Collection<ItemStack> drops = new ArrayList<>();
+
+                    for (int x = xStart; x <= xEnd; x++) {
+                        for (int y = yStart; y <= yEnd; y++) {
+                            block:
+                            for (int z = zStart; z <= zEnd; z++) {
+                                Block relative = origin.getRelative(x, y, z);
+                                for (Predicate<Block> predicateWhitelist : whitelist) {
+                                    if (predicateWhitelist.test(relative)) {
+                                        for (Predicate<Block> predicateBlacklist : blacklist) {
+                                            if (predicateBlacklist.test(relative)) {
+                                                continue block;
+                                            }
+                                        }
+                                        //Testing claim
+                                        Location relativeLocation = relative.getLocation();
+                                        if (Utils.isInsideClaim(player, relativeLocation) || Utils.isWilderness(relativeLocation)) {
+                                            drops.addAll(relative.getDrops(heldItem));
+                                            relative.setType(Material.AIR);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    dropAllItemStacks(drops, location.getWorld(), location);
+                })
+                .withPermission(this.getPermission())
+                .withAliases(this.getCommandAliases())
+                .register(this.getNamespace());
+
+            /**
              * Breaks Blocks in Direction Player is Facing with GriefPrevention, Define Predicates in List Format on Command, Custom Block Drop
              * @author DuneSciFye
              * @since 1.0.0
