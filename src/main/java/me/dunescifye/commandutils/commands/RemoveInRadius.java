c0dd2ca5f5122515ee6a6c60dfd4a755739d08f4
DuneSciFye <65089425+DuneSciFye@users.noreply.github.com>
2024-08-28 06:14:18 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/RemoveInRadius.java
+++ src/main/java/me/dunescifye/commandutils/commands/RemoveInRadius.java
@@ -6,6 +6,7 @@ import me.dunescifye.commandutils.CommandUtils;
 import me.dunescifye.commandutils.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -13,6 +14,8 @@ import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
 
 import static me.dunescifye.commandutils.utils.Utils.dropAllItemStacks;
 import static org.bukkit.Material.AIR;
@@ -34,7 +37,7 @@ public class RemoveInRadius extends Command implements Registerable {
         if (CommandUtils.griefPreventionEnabled) {
 
             /**
-             * Breaks Blocks in Radius with GriefPrevention Support, Breaks all Blocks
+             * Remove Blocks in Radius with GriefPrevention Support
              * @author DuneSciFye
              * @since 1.0.0
              * @param World World of the Blocks
@@ -67,6 +70,65 @@ public class RemoveInRadius extends Command implements Registerable {
                         }
                     }
 
+                })
+                .withPermission(this.getPermission())
+                .withAliases(this.getCommandAliases())
+                .register(this.getNamespace());
+
+            /**
+             * Removes Blocks in Radius with GriefPrevention Support, Command Defined Predicates
+             * @author DuneSciFye
+             * @since 1.0.0
+             * @param World World of the Blocks
+             * @param Location Location of the Center Block
+             * @param Player Player who is Breaking the Blocks
+             * @param Radius Radius to Break Blocks In
+             * @param whitelist Literal Arg
+             * @param Predicates List of Predicates
+             */
+            new CommandAPICommand("removeinradius")
+                .withArguments(worldArg)
+                .withArguments(locArg)
+                .withArguments(playerArg)
+                .withArguments(radiusArg)
+                .withArguments(whitelistArg)
+                .withArguments(new ListArgumentBuilder<String>("Whitelisted Blocks")
+                    .withList(Utils.getPredicatesList())
+                    .withStringMapper()
+                    .buildText())
+                .executes((sender, args) -> {
+                    List<Predicate<Block>> whitelist = new ArrayList<>(), blacklist = new ArrayList<>();
+                    Utils.stringListToPredicate(args.getUnchecked("Whitelisted Blocks"), whitelist, blacklist);
+
+                    World world = Bukkit.getWorld(args.getByArgument(worldArg));
+                    Location location = args.getByArgument(locArg);
+                    Block origin = world.getBlockAt(location);
+                    Player player = args.getByArgument(playerArg);
+                    int radius = args.getByArgument(radiusArg);
+
+                    for (int x = -radius; x <= radius; x++) {
+                        for (int y = -radius; y <= radius; y++) {
+                            block:
+                            for (int z = -radius; z <= radius; z++) {
+                                Block relative = origin.getRelative(x, y, z);
+                                for (Predicate<Block> predicateWhitelist : whitelist) {
+                                    if (predicateWhitelist.test(relative)) {
+                                        for (Predicate<Block> predicateBlacklist : blacklist) {
+                                            if (predicateBlacklist.test(relative)) {
+                                                continue block;
+                                            }
+                                        }
+                                        //Testing claim
+                                        Location relativeLocation = relative.getLocation();
+                                        if (Utils.isInsideClaim(player, relativeLocation) || Utils.isWilderness(relativeLocation)) {
+                                            relative.setType(Material.AIR);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
                 })
                 .withPermission(this.getPermission())
                 .withAliases(this.getCommandAliases())
