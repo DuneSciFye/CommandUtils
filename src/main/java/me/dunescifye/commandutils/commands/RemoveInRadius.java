ca5adc370aadbdf84af6b57f0c04244cf27a6f89
DuneSciFye <dunescifye@gmail.com>
2024-08-28 14:36:29 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/RemoveInRadius.java
+++ src/main/java/me/dunescifye/commandutils/commands/RemoveInRadius.java
@@ -388,6 +388,72 @@ public class RemoveInRadius extends Command implements Registerable {
                 .register(this.getNamespace());
 
             /**
+             * Removes Blocks in Radius with GriefPrevention Support, Command Defined Predicates
+             * @author DuneSciFye
+             * @since 1.0.3
+             * @param World World of the Blocks
+             * @param Location Location of the Center Block
+             * @param X Direction in X to Break in
+             * @param Y Direction in Y to Break in
+             * @param Z Direction in Z to Break in
+             * @param Player Player who is Breaking the Blocks
+             * @param whitelist Literal Arg
+             * @param Predicates List of Predicates
+             */
+            new CommandAPICommand("removeinradius")
+                .withArguments(locArg)
+                .withArguments(xArg)
+                .withArguments(yArg)
+                .withArguments(zArg)
+                .withArguments(playerArg)
+                .withOptionalArguments(whitelistArg
+                    .combineWith(
+                        new ListArgumentBuilder<String>("Whitelisted Blocks")
+                            .withList(Utils.getPredicatesList())
+                            .withStringMapper()
+                            .buildText()
+                    )
+                )
+                .executes((sender, args) -> {
+                    List<Predicate<Block>> whitelist = new ArrayList<>(), blacklist = new ArrayList<>();
+                    Utils.stringListToPredicate(args.getUnchecked("Whitelisted Blocks"), whitelist, blacklist);
+
+                    Location location = args.getByArgument(locArg);
+                    Block origin = location.getBlock();
+                    Player player = args.getByArgument(playerArg);
+                    int xRadius = args.getByArgument(xArg);
+                    int yRadius = args.getByArgument(yArg);
+                    int zRadius = args.getByArgument(zArg);
+
+                    for (int x = -xRadius; x <= xRadius; x++) {
+                        for (int y = -yRadius; y <= yRadius; y++) {
+                            block:
+                            for (int z = -zRadius; z <= zRadius; z++) {
+                                Block relative = origin.getRelative(x, y, z);
+                                for (Predicate<Block> predicateWhitelist : whitelist) {
+                                    if (predicateWhitelist.test(relative)) {
+                                        for (Predicate<Block> predicateBlacklist : blacklist) {
+                                            if (predicateBlacklist.test(relative)) {
+                                                continue block;
+                                            }
+                                        }
+                                        //Testing claim
+                                        Location relativeLocation = relative.getLocation();
+                                        if (Utils.isInsideClaim(player, relativeLocation) || Utils.isWilderness(relativeLocation)) {
+                                            relative.setType(Material.AIR);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                })
+                .withPermission(this.getPermission())
+                .withAliases(this.getCommandAliases())
+                .register(this.getNamespace());
+
+            /**
              * Removes Blocks in Radius with GriefPrevention Support, Config Defined Predicates
              * @author DuneSciFye
              * @since 1.0.3
