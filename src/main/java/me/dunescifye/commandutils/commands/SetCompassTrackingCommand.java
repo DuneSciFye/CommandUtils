36bfedc01066c92930d3838cc238a84a64e9e371
DuneSciFye <dunescifye@gmail.com>
2024-08-31 19:23:29 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/SetCompassTrackingCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/SetCompassTrackingCommand.java
@@ -2,6 +2,7 @@ package me.dunescifye.commandutils.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.*;
+import me.dunescifye.commandutils.utils.Utils;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
@@ -22,6 +23,7 @@ public class SetCompassTrackingCommand extends Command implements Registerable {
 
         PlayerArgument playerArg = new PlayerArgument("Player");
         IntegerArgument slotArg = new IntegerArgument("Slot");
+        MultiLiteralArgument textSlotArg = new MultiLiteralArgument("Slot", "main", "mainhand", "off", "offhand", "cursor");
         StringArgument worldArg = new StringArgument("World");
         LocationArgument locArg = new LocationArgument("Location");
 
@@ -45,6 +47,64 @@ public class SetCompassTrackingCommand extends Command implements Registerable {
                 ItemStack item = p.getInventory().getItem(args.getByArgument(slotArg));
                 Location loc = args.getByArgument(locArg);
                 loc.setWorld(Bukkit.getWorld(args.getByArgument(worldArg)));
+
+                if (item.getItemMeta() instanceof CompassMeta compassMeta) {
+                    compassMeta.setLodestone(loc);
+                }
+
+            })
+            .withPermission(this.getPermission())
+            .withAliases(this.getCommandAliases())
+            .register(this.getNamespace());
+
+        /**
+         * Sets a Compass to Track Location
+         * @author DuneSciFye
+         * @since 1.0.5
+         * @param Player Player to get Inventory
+         * @param Slot Text Slot to get Compass from
+         * @param World World to get Tracking Location
+         * @param Location Location
+         */
+        new CommandAPICommand("setcompasstracking")
+            .withArguments(playerArg)
+            .withArguments(textSlotArg)
+            .withArguments(worldArg)
+            .withArguments(locArg)
+            .executes((sender, args) -> {
+                ItemStack item = Utils.getInvItem(
+                    args.getByArgument(playerArg),
+                    args.getByArgument(textSlotArg)
+                );
+                Location loc = args.getByArgument(locArg);
+                loc.setWorld(Bukkit.getWorld(args.getByArgument(worldArg)));
+
+                if (item.getItemMeta() instanceof CompassMeta compassMeta) {
+                    compassMeta.setLodestone(loc);
+                }
+
+            })
+            .withPermission(this.getPermission())
+            .withAliases(this.getCommandAliases())
+            .register(this.getNamespace());
+
+        /**
+         * Sets a Compass to Track Location
+         * @author DuneSciFye
+         * @since 1.0.5
+         * @param Player Player to get Inventory
+         * @param Slot Integer Slot to get Compass from
+         * @param Location Location
+         */
+        new CommandAPICommand("setcompasstracking")
+            .withArguments(playerArg)
+            .withArguments(slotArg)
+            .withArguments(locArg)
+            .executes((sender, args) -> {
+
+                Player p = args.getByArgument(playerArg);
+                ItemStack item = p.getInventory().getItem(args.getByArgument(slotArg));
+                Location loc = args.getByArgument(locArg);
 
                 if (item.getItemMeta() instanceof CompassMeta compassMeta) {
                     compassMeta.setLodestone(loc);
