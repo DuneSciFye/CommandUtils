62fc1401f5fbfcca09926750b5e792d77828860b
DuneSciFye <dunescifye@gmail.com>
2024-08-31 19:29:20 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/SpawnGuardianBeamCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/SpawnGuardianBeamCommand.java
@@ -7,6 +7,9 @@ import dev.jorel.commandapi.arguments.LocationArgument;
 import dev.jorel.commandapi.arguments.StringArgument;
 import me.dunescifye.commandutils.CommandUtils;
 import me.dunescifye.commandutils.utils.Laser;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 
 public class SpawnGuardianBeamCommand extends Command implements Registerable {
     @SuppressWarnings("ConstantConditions")
@@ -24,10 +27,48 @@ public class SpawnGuardianBeamCommand extends Command implements Registerable {
         IntegerArgument distanceArg = new IntegerArgument("Distance");
 
         /**
-         * Summons a Guardian Beam between two Location
+         * Summons a Guardian Beam between two Locations
          * @author DuneSciFye
          * @since 1.0.5
          * @param World World of the Locations
+         * @param Location1 Coordinates of First Location
+         * @param Location2 Coordinates of Second Location
+         * @param Duration How Long Laser Stays for
+         * @param Distance How Long Laser is
+         */
+        new CommandAPICommand("spawnguardianbeam")
+            .withArguments(worldArg)
+            .withArguments(loc1Arg)
+            .withArguments(loc2Arg)
+            .withArguments(durationArg)
+            .withArguments(distanceArg)
+            .executes((sender, args) -> {
+                Location location1 = args.getByArgument(loc1Arg);
+                Location location2 = args.getByArgument(loc2Arg);
+                World world = Bukkit.getWorld(args.getByArgument(worldArg));
+                location1.setWorld(world);
+                location2.setWorld(world);
+                try {
+                    Laser laser = new Laser.GuardianLaser(
+                        location1,
+                        location2,
+                        args.getByArgument(durationArg),
+                        args.getByArgument(distanceArg)
+                    );
+                    laser.start(CommandUtils.getInstance());
+                } catch (
+                    ReflectiveOperationException ignored) {
+                }
+
+            })
+            .withPermission(this.getPermission())
+            .withAliases(this.getCommandAliases())
+            .register(this.getNamespace());
+
+        /**
+         * Summons a Guardian Beam between two Locations
+         * @author DuneSciFye
+         * @since 1.0.5
          * @param Location1 Coordinates of First Location
          * @param Location2 Coordinates of Second Location
          * @param Duration How Long Laser Stays for
@@ -59,6 +100,40 @@ public class SpawnGuardianBeamCommand extends Command implements Registerable {
 
         /**
          * Summons a Guardian Beam between two Entities
+         * @author DuneSciFye
+         * @since 1.0.5
+         * @param World World of the Locations
+         * @param Entity1 First Entity
+         * @param Entity2 Second Entity
+         * @param Duration How Long Laser Stays for
+         * @param Distance How Long Laser is
+         */
+        new CommandAPICommand("spawnguardianbeam")
+            .withArguments(worldArg)
+            .withArguments(entity1Arg)
+            .withArguments(entity2Arg)
+            .withArguments(durationArg)
+            .withArguments(distanceArg)
+            .executes((sender, args) -> {
+                try {
+                    Laser laser = new Laser.GuardianLaser(
+                        args.getByArgument(entity1Arg).getLocation(),
+                        args.getByArgument(entity2Arg).getLocation(),
+                        args.getByArgument(durationArg),
+                        args.getByArgument(distanceArg)
+                    );
+                    laser.start(CommandUtils.getInstance());
+                } catch (
+                    ReflectiveOperationException ignored) {
+                }
+
+            })
+            .withPermission(this.getPermission())
+            .withAliases(this.getCommandAliases())
+            .register(this.getNamespace());
+
+        /**
+         * Summons a Guardian Beam between an Entity and Location
          * @author DuneSciFye
          * @since 1.0.5
          * @param World World of the Locations
