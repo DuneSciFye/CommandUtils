a8eb6f7f9fdb2676f2c39413e6a59e8d5cde4023
DuneSciFye <dunescifye@gmail.com>
2024-08-31 19:40:45 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/BreakInFacingCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/BreakInFacingCommand.java
@@ -348,6 +348,106 @@ public class BreakInFacingCommand extends Command implements Registerable {
                 .withAliases(this.getCommandAliases())
                 .register(this.getNamespace());
 
+            /**
+             * Breaks Blocks in Direction Player is Facing with GriefPrevention, Define Predicates in List Format on Command
+             * @author DuneSciFye
+             * @since 1.0.0
+             * @param World World of the Blocks
+             * @param Location Location of the Center Block
+             * @param Player Player who is Breaking the Blocks
+             * @param Radius Radius to Break Blocks In
+             * @param Depth Number of Blocks to Break Forward in
+             * @param whitelist Literal Argument
+             * @param Predicates List of Predicates
+             */
+            new CommandAPICommand("breakinfacing")
+                .withArguments(worldArg)
+                .withArguments(locArg)
+                .withArguments(playerArg)
+                .withArguments(xArg)
+                .withArguments(yArg)
+                .withArguments(zArg)
+                .withArguments(depthArg)
+                .withArguments(whitelistArg)
+                .withArguments(new ListArgumentBuilder<String>("Whitelisted Blocks")
+                    .withList(Utils.getPredicatesList())
+                    .withStringMapper()
+                    .buildText())
+                .executes((sender, args) -> {
+                    List<Predicate<Block>> whitelist = new ArrayList<>(), blacklist = new ArrayList<>();
+                    Utils.stringListToPredicate(args.getUnchecked("Whitelisted Blocks"), whitelist, blacklist);
+
+                    World world = Bukkit.getWorld(args.getByArgument(worldArg));
+                    Location location = args.getByArgument(locArg);
+                    Block origin = world.getBlockAt(location);
+                    int radius = args.getByArgument(radiusArg);
+                    Player player = args.getByArgument(playerArg);
+                    ItemStack heldItem = player.getInventory().getItemInMainHand();
+                    int depth = args.getByArgument(depthArg);
+
+                    depth = depth < 1 ? 1 : depth -1;
+                    double pitch = player.getLocation().getPitch();
+                    int xStart = -radius, yStart = -radius, zStart = -radius, xEnd = radius, yEnd = radius, zEnd = radius;
+                    if (pitch < -45) {
+                        yStart = 0;
+                        yEnd = depth;
+                    } else if (pitch > 45) {
+                        yStart = -depth;
+                        yEnd = 0;
+                    } else {
+                        switch (player.getFacing()) {
+                            case NORTH -> {
+                                zStart = -depth;
+                                zEnd = 0;
+                            }
+                            case SOUTH -> {
+                                zStart = 0;
+                                zEnd = depth;
+                            }
+                            case WEST -> {
+                                xStart = -depth;
+                                xEnd = 0;
+                            }
+                            case EAST -> {
+                                xStart = 0;
+                                xEnd = depth;
+                            }
+                        }
+                    }
+
+                    Collection<ItemStack> drops = new ArrayList<>();
+
+                    for (int x = xStart; x <= xEnd; x++) {
+                        for (int y = yStart; y <= yEnd; y++) {
+                            block:
+                            for (int z = zStart; z <= zEnd; z++) {
+                                Block relative = origin.getRelative(x, y, z);
+                                for (Predicate<Block> predicateWhitelist : whitelist) {
+                                    if (predicateWhitelist.test(relative)) {
+                                        for (Predicate<Block> predicateBlacklist : blacklist) {
+                                            if (predicateBlacklist.test(relative)) {
+                                                continue block;
+                                            }
+                                        }
+                                        //Testing claim
+                                        Location relativeLocation = relative.getLocation();
+                                        if (Utils.isInsideClaim(player, relativeLocation) || Utils.isWilderness(relativeLocation)) {
+                                            drops.addAll(relative.getDrops(heldItem));
+                                            relative.setType(Material.AIR);
+                                        }
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    dropAllItemStacks(drops, world, location);
+                })
+                .withPermission(this.getPermission())
+                .withAliases(this.getCommandAliases())
+                .register(this.getNamespace());
+
     }
 
 }
