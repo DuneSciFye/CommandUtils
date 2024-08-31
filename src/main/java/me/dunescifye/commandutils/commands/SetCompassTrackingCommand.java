b5f7a315d8056afa670e9baea020701df4d56bc3
DuneSciFye <dunescifye@gmail.com>
2024-08-31 19:21:40 -0400

    Work on Commands

---
+++ src/main/java/me/dunescifye/commandutils/commands/SetCompassTrackingCommand.java
@@ -0,0 +1,59 @@
+package me.dunescifye.commandutils.commands;
+
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.arguments.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CompassMeta;
+
+import java.util.Collection;
+
+public class SetCompassTrackingCommand extends Command implements Registerable {
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public void register() {
+
+        if (!this.getEnabled()) return;
+
+        PlayerArgument playerArg = new PlayerArgument("Player");
+        IntegerArgument slotArg = new IntegerArgument("Slot");
+        StringArgument worldArg = new StringArgument("World");
+        LocationArgument locArg = new LocationArgument("Location");
+
+        /**
+         * Sets a Compass to Track Location
+         * @author DuneSciFye
+         * @since 1.0.5
+         * @param Player Player to get Inventory
+         * @param Slot Integer Slot to get Compass from
+         * @param World World to get Tracking Location
+         * @param Location Location
+         */
+        new CommandAPICommand("setcompasstracking")
+            .withArguments(playerArg)
+            .withArguments(slotArg)
+            .withArguments(worldArg)
+            .withArguments(locArg)
+            .executes((sender, args) -> {
+
+                Player p = args.getByArgument(playerArg);
+                ItemStack item = p.getInventory().getItem(args.getByArgument(slotArg));
+                Location loc = args.getByArgument(locArg);
+                loc.setWorld(Bukkit.getWorld(args.getByArgument(worldArg)));
+
+                if (item.getItemMeta() instanceof CompassMeta compassMeta) {
+                    compassMeta.setLodestone(loc);
+                }
+
+            })
+            .withPermission(this.getPermission())
+            .withAliases(this.getCommandAliases())
+            .register(this.getNamespace());
+
+    }
+}
