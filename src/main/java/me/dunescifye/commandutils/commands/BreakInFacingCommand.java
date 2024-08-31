70bc7d0374992ae8eca4ce86861cc1719f3b7a4b
DuneSciFye <dunescifye@gmail.com>
2024-08-31 19:40:00 -0400

    Work on Commands

--- src/main/java/me/dunescifye/commandutils/commands/BreakInFacingCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/BreakInFacingCommand.java
@@ -270,6 +270,84 @@ public class BreakInFacingCommand extends Command implements Registerable {
                 .withAliases(this.getCommandAliases())
                 .register(this.getNamespace());
 
+            /**
+             * Breaks Blocks in Direction Player is Facing with GriefPrevention, Breaks all Blocks
+             * @author DuneSciFye
+             * @since 1.0.0
+             * @param Location Location of the Center Block
+             * @param Player Player who is Breaking the Blocks
+             * @param X Direction in X to Break in
+             * @param Y Direction in Y to Break in
+             * @param Z Direction in Z to Break in
+             * @param Depth Number of Blocks to Break Forward in
+             */
+            new CommandAPICommand("breakinfacing")
+                .withArguments(locArg)
+                .withArguments(playerArg)
+                .withArguments(radiusArg)
+                .withArguments(depthArg)
+                .executes((sender, args) -> {
+                    Location location = args.getByArgument(locArg);
+                    Block block = location.getBlock();
+                    int xRadius = args.getByArgument(xArg);
+                    int yRadius = args.getByArgument(yArg);
+                    int zRadius = args.getByArgument(zArg);
+                    Player player = args.getByArgument(playerArg);
+                    ItemStack heldItem = player.getInventory().getItemInMainHand();
+                    int depth = args.getByArgument(depthArg);
+
+                    depth = depth < 1 ? 1 : depth -1;
+                    double pitch = player.getLocation().getPitch();
+                    int xStart = -xRadius, yStart = -yRadius, zStart = -zRadius, xEnd = xRadius, yEnd = yRadius, zEnd = zRadius;
+                    if (pitch < -45) {
+                        yStart = 0;
+                        yEnd = depth;
+                    } else if (pitch > 45) {
+                        yStart = -depth;
+                        yEnd = 0;
+                    } else {
+                        switch (player.getFacing()) {
+                            case NORTH -> {
+                                zStart = -depth;
+                                zEnd = 0;
+                            }
+                            case SOUTH -> {
+                                zStart = 0;
+                                zEnd = depth;
+                            }
+                            case WEST -> {
+                                xStart = -depth;
+                                xEnd = 0;
+                            }
+                            case EAST -> {
+                                xStart = 0;
+                                xEnd = depth;
+                            }
+                        }
+                    }
+
+                    Collection<ItemStack> drops = new ArrayList<>();
+
+                    for (int x = xStart; x <= xEnd; x++) {
+                        for (int y = yStart; y <= yEnd; y++) {
+                            for (int z = zStart; z <= zEnd; z++) {
+                                Block relative = block.getRelative(x, y, z);
+                                //Testing claim
+                                Location relativeLocation = relative.getLocation();
+                                if (Utils.isInsideClaim(player, relativeLocation) || Utils.isWilderness(relativeLocation)) {
+                                    drops.addAll(relative.getDrops(heldItem));
+                                    relative.setType(Material.AIR);
+                                }
+                            }
+                        }
+                    }
+
+                    dropAllItemStacks(drops, location.getWorld(), location);
+                })
+                .withPermission(this.getPermission())
+                .withAliases(this.getCommandAliases())
+                .register(this.getNamespace());
+
     }
 
 }
