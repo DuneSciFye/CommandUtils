06bd7ad95876310968c18c00e98ce0332b1df2db
DuneSciFye <65089425+DuneSciFye@users.noreply.github.com>
2025-12-20 03:01:18 -0500

    Trigger shear event in ShearEntityCommand

--- src/main/java/me/dunescifye/commandutils/commands/ShearEntityCommand.java
+++ src/main/java/me/dunescifye/commandutils/commands/ShearEntityCommand.java
@@ -2,8 +2,14 @@ package me.dunescifye.commandutils.commands;
 
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.EntitySelectorArgument;
+import dev.jorel.commandapi.executors.ExecutorType;
 import io.papermc.paper.entity.Shearable;
+import me.dunescifye.commandutils.utils.ArgumentUtils;
+import net.kyori.adventure.sound.Sound;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerShearEntityEvent;
 
 import java.util.Collection;
 
@@ -16,6 +22,20 @@ public class ShearEntityCommand extends Command implements Registerable
 
         new CommandAPICommand("shearentity")
             .withArguments(entitiesArg)
+            .executes((sender, args) -> {
+                Collection<Entity> entities = args.getByArgument(entitiesArg);
+                Player player = ArgumentUtils.getPlayer(sender);
+
+                for (Entity entity : entities) {
+                    if (entity instanceof Shearable shearable) {
+                        shearable.shear();
+                        if (player != null) {
+                            PlayerShearEntityEvent event = new PlayerShearEntityEvent(player, entity);
+                            Bukkit.getPluginManager().callEvent(event);
+                        }
+                    }
+                }
+            }, ExecutorType.PLAYER, ExecutorType.PROXY)
             .executes((sender, args) -> {
                 Collection<Entity> entities = args.getByArgument(entitiesArg);
 
